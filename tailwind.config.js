9784dfca0a8c5d0e8ae349439e2c6aea42252583
Josh Boys <josh.boys@gmail.com>
2024-09-16 12:15:49 +0930

    Refactor and use custom project colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Outfit", "sans-serif"],
       },
+      colors: {
+        slate: {
+          300: "#d5e1ef",
+          500: "#68778d",
+          900: "#1f314f",
+        },
+      }
     }
   },
   plugins: [],
