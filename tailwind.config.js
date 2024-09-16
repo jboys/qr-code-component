0ecec0c4bb56cd2f5e33788cc627a8314d9916f8
Josh Boys <josh.boys@gmail.com>
2024-09-16 12:09:21 +0930

    Refactor and use custom project colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         sans: ["Outfit", "sans-serif"],
       },
+      colors: {
+        slate: {
+          300: "#d5e1ef",
+          500: "#68778d",
+          900: "#1f314f",
+        },
+      }
     }
   },
   plugins: [],
